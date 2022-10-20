613735d69f933a470357a620cf46a1d35a382abd
njerikungu <kungumaryanne@gmail.com>
2022-10-20 10:22:08 -0700

    headerfile

---
+++ headerfile.h
@@ -0,0 +1,41 @@
+#ifndef _PRINTF_H
+#define _PRINTF_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+* struct format - Structure name template for funcs
+* @symbol: format specifier
+* @func: function called after the format specifier
+*/
+
+
+struct format
+{
+	char *symbol;
+	int (*func)(va_list);
+};
+
+/**
+* format_t - struct nickname
+*/
+typedef struct format format_t;
+
+/* functions */
+int _printf(const char *format, ...);
+int _putchar(char c);
+int (*get_func(char s))(va_list);
+int print_c(va_list arg);
+int print_s(va_list arg);
+int print_int(va_list arg);
+int print_i(va_list arg);
+int print_binary(va_list arg);
+int print_octal(va_list arg);
+int print_unint(va_list arg);
+int print_hex(va_list arg);
+int print_hexlow(va_list arg);
+
+
+#endif
