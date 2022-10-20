91b9c307b79acc75353ac064f4bf19aaab34bea1
JeremyWarui <jerywarne7@gmail.com>
2022-10-20 16:53:05 -0700

    update hex uppercase

--- hex.c
+++ hex.c
@@ -6,40 +6,35 @@
 * Return: no of arguments
 */
 
-
 int print_hex(va_list arg)
 {
-char arr[100];
-int i, j, temp, len;
-unsigned int num;
+	char arr[8];
+	int i, j, temp, len;
+	unsigned int num;
 
-num = va_arg(arg, int);
-i = 0;
+	num = va_arg(arg, int);
+	i = 0;
 
-while (num > 0)
-{
-temp = 0;
+	while (num > 0)
+	{
+		temp = 0;
+		temp = num % 16;
 
-temp = num % 16;
-
-if (temp < 10)
-{
-arr[i] = 48 + temp;
-i++;
+		if (temp < 10)
+		{
+			arr[i] = 48 + temp;
+			i++;
+		}
+		else
+		{
+			arr[i] = 55 + temp;
+			i++;
+		}
+		num /= 16;
 	}
-else
-{
-arr[i] = 55 + temp;
-	i++;
-			}
+	len = i;
 
-num /= 16;
-}
-
-len = i;
-
-for (j = len - 1; j >= 0; j--)
-_putchar((arr[j]));
-
-return (i);
+	for (j = len - 1; j >= 0; j--)
+		_putchar(arr[j]);
+	return (i);
 }
