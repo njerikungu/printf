ebd87e711bcf136640774564db76de1b5363f7ca
JeremyWarui <jerywarne7@gmail.com>
2022-10-20 16:48:30 -0700

    update hex lowercase

--- hex_low.c
+++ hex_low.c
@@ -8,37 +8,34 @@
 
 int print_hexlow(va_list arg)
 {
+	int i, j, temp, len;
+	unsigned int num;
+	char arr[8];
 
-int i, j, temp, len;
-unsigned int num;
-char arr[100];
+	num = va_arg(arg, int);
+	i = 0;
 
-num = va_arg(arg, int);
-i = 0;
+	while (num > 0)
+	{
+		temp = 0;
+		temp = num % 16;
 
-while (num > 0)
-{
-temp = 0;
-temp = num % 16;
-
-if (temp < 10)
-{
-arr[i] = 48 + temp;
-	i++;
-			}
-else
-{
-arr[i] = 87 + temp;
+		if (temp < 10)
+		{
+			arr[i] = 48 + temp;
 			i++;
-					}
+		}
+		else
+		{
+			arr[i] = 87 + temp;
+			i++;
+		}
+		num /= 16;
+	}
+	len = i;
 
-num /= 16;
-}
+	for (j = len - 1; j >= 0; j--)
+		_putchar(arr[j]);
 
-len = i;
-
-for (j = len - 1; j >= 0; j--)
-_putchar(arr[j]);
-
-return (i);
+	return (i);
 }
