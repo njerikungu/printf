ea752ffd4fb2fa25edbe3dded7fd4b8b49b8eace
njerikungu <kungumaryanne@gmail.com>
2022-10-20 15:40:33 -0700

    hexagon Lowercase

---
+++ hex_low.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+* print_hexlow - print hex in lowercase
+* @arg: list of argument
+* Return: no of arguments
+*/
+
+int print_hexlow(va_list arg)
+{
+
+int i, j, temp, len;
+unsigned int num;
+char arr[100];
+
+num = va_arg(arg, int);
+i = 0;
+
+while (num > 0)
+{
+temp = 0;
+temp = num % 16;
+
+if (temp < 10)
+{
+arr[i] = 48 + temp;
+	i++;
+			}
+else
+{
+arr[i] = 87 + temp;
+			i++;
+					}
+
+num /= 16;
+}
+
+len = i;
+
+for (j = len - 1; j >= 0; j--)
+_putchar(arr[j]);
+
+return (i);
+}
