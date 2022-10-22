b8fc73f2432726e3aaf5db14673b434601a3e270
JeremyWarui <jerywarne7@gmail.com>
2022-10-22 11:37:45 -0700

    fix length on printf

--- printf.c
+++ printf.c
@@ -8,43 +8,41 @@
 
 int _printf(const char *format, ...)
 {
-	int i = 0, len;
-	va_list arg;
-	int (*func)(va_list);
+	int len = -1;
 
-	if (format == NULL)
-		return (-1);
-
-	va_start(arg, format);
-	len = 0;
-
-	for (i = 0; format[i] != '\0'; i++)
+	if (format != NULL)
 	{
-		if (format[i] == '%')
+		va_list arg;
+		int (*func)(va_list);
+		int i;
+
+		va_start(arg, format);
+		len = 0;
+		for (i = 0; format[i] != '\0'; i++)
 		{
-			if (format[i + 1] == '%')
+			if (format[i] == '%')
 			{
-				_putchar(format[i]);
-				i++;
-				len++;
-			}
-			else if (format[i + 1] != '\0')
-			{
-				func = get_func(format[i + 1]);
-				if (func)
-					len += func(arg);
-				else if (!func && format[i + 1] != ' ')
+				if (format[i + 1] == '%')
 				{
-					_putchar(format[i]);
-					_putchar(format[i + 1]);
-					len += 2;
+					len += _putchar(format[i]);
+					i++;
 				}
-				i++;
+				else if (format[i + 1] != '\0')
+				{
+					func = get_func(format[i + 1]);
+					if (func)
+						len += func(arg);
+					else if (!func && format[i + 1] != ' ')
+					{
+						len += (_putchar(format[i]) + _putchar(format[i + 1]));
+					}
+					i++;
+				}
 			}
+			else
+				len += _putchar(format[i]);
 		}
-		else
-			len += _putchar(format[i]);
+		va_end(arg);
 	}
-	va_end(arg);
 	return (len);
 }
