67e04e78b5964250e0f4953aa3271b89e183007a
JeremyWarui <jerywarne7@gmail.com>
2022-10-22 12:07:55 -0700

    update printf

--- printf.c
+++ printf.c
@@ -8,39 +8,39 @@
 
 int _printf(const char *format, ...)
 {
-	int len = -1;
+	va_list arg;
+	int (*func)(va_list);
+	unsigned int i = 0, len = 0;
 
-	if (format != NULL)
+	if (format == NULL)
+		return (-1);
+
+	va_start(arg, format);
+	while (format[i])
 	{
-		va_list arg;
-		int (*func)(va_list arg);
-		int i;
-
-		va_start(arg, format);
-		len = 0;
-		for (i = 0; format[i]; i++)
+		for (; format[i] != '%' && format[i]; i++)
 		{
-			if (format[i] == '%')
-			{
-				if (format[i + 1] == '%')
-				{
-					len += _putchar(format[i]);
-					i++;
-				}
-				else if (format[i + 1] != '\0')
-				{
-					func = get_func(format[i + 1]);
-					if (func)
-						len += func(arg);
-					else
-						len += (_putchar(format[i]) + _putchar(format[i + 1]));
-					i++;
-				}
-			}
-			else
-				len += _putchar(format[i]);
+			_putchar(format[i]);
+			len++;
 		}
-		va_end(arg);
+		if (!format[i])
+			return (len);
+		func = get_func(format[i + 1]);
+		if (func != NULL)
+		{
+			len += func(arg);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		len++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
 	}
+	va_end(arg);
 	return (len);
 }
